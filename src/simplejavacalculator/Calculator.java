410460ffc6da79c1dd283c5863116ca2976d12d8
Insafsalma <58422379+Insafsalma@users.noreply.github.com>
2020-12-14 17:24:50 +0100

    Update Calculator.java

--- src/simplejavacalculator/Calculator.java
+++ src/simplejavacalculator/Calculator.java
@@ -8,7 +8,7 @@
  * @copyright   Copyright Pierre-Henry SORIA, All Rights Reserved.
  * @license     Apache (http://www.apache.org/licenses/LICENSE-2.0)
  */
-//just test
+
 
 
 package simplejavacalculator;
